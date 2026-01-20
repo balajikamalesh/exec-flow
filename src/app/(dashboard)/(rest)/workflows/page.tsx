c7bb67fbefdf36d0e3f2c8cd0969fa67091aee15
balajikamalesh <abalajikamalesh@gmail.com>
2026-01-20 17:15:00 +0530

    feat(workflow): Add workflow crud flow

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,12 +1,31 @@
+import {
+  WorkflowsContainer,
+  WorkflowsList,
+} from "@/features/workflows/components/workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
-import React from "react";
+import { HydrateClient } from "@/trpc/server";
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 type Props = {};
 
 const WorkflowsPage = async (props: Props) => {
   await requireAuth();
 
-  return <div></div>;
+  prefetchWorkflows();
+
+  return (
+    <WorkflowsContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<div>Failed to load workflows.</div>}>
+          <Suspense fallback={<div>Loading workflows...</div>}>
+            <WorkflowsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkflowsContainer>
+  );
 };
 
 export default WorkflowsPage;
---
+++ src/components/upgrade-modal.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogDescription,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
+
+interface UpgradeModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const UpgradeModal = ({ open, onOpenChange }: UpgradeModalProps) => {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Upgrade to Pro</AlertDialogTitle>
+          <AlertDialogDescription>
+            You need to upgrade your account to access this feature. Upgrade to
+            Pro for additional benefits and features.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => authClient.checkout({ slug: "ExecFlow-Pro" })}
+          >
+            Upgrade Now
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
